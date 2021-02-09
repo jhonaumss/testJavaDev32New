4ab3b345d4fd41b3e6c38d26c4500915e0c49a8a
Jhonatan Sanchez <Jhonatan.Sanchez@fundacion-jala.org>
2021-02-09 19:37:08 -0400

    implemented tests to new validators

--- src/test/java/org/fundacionjala/app/quizz/model/validator/ValidatorIT.java
+++ src/test/java/org/fundacionjala/app/quizz/model/validator/ValidatorIT.java
@@ -18,4 +18,32 @@ public class ValidatorIT {
         Assert.assertEquals(DateValidator.class, dateValidator.getClass());
         Assert.assertTrue(errors.isEmpty());
     }
+    @Test
+    public void testMaxLengthValidator(){
+        List<String> errors = new ArrayList<>();
+        Validator maxLengthValidator = ValidatorType.MAX_LENGTH.getValidator();
+        String conditionValue = "5";
+        maxLengthValidator.validate("esto es el mensaje",conditionValue, errors);
+        Assert.assertEquals(MaxLengthValidator.class, maxLengthValidator.getClass());
+        Assert.assertEquals(errors.get(0),"The value length must not exceed "+conditionValue);
+    }
+
+    @Test
+    public void testMaxValidator(){
+        List<String> errors = new ArrayList<>();
+        Validator maxValidator = ValidatorType.MAX.getValidator();
+        String conditionValue = "5";
+        maxValidator.validate("8",conditionValue, errors);
+        Assert.assertEquals(MaxValidator.class, maxValidator.getClass());
+        Assert.assertFalse(errors.isEmpty());
+    }
+    //OnlyUpperCaseTextValidator
+    @Test
+    public void testOnlyUpperCaseTextValidator(){
+        List<String> errors = new ArrayList<>();
+        Validator onlyUpperCaseTextValidator = ValidatorType.ONLY_UPPERCASE_TEXT.getValidator();
+        onlyUpperCaseTextValidator.validate("HOLA",null, errors);
+        Assert.assertEquals(OnlyUpperCaseTextValidator.class, onlyUpperCaseTextValidator.getClass());
+        Assert.assertTrue(errors.isEmpty());
+    }
 }
